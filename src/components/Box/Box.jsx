08519a663492857f9d699c4aa79379183b53bfef
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-07-19 22:42:48 +0300

    Box.jsx

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,16 +1,33 @@
 import { BOX_SIZE } from "../../constants";
 import css from "./Box.module.css";
-const Box = ({ isFilled }) => {
+import PropTypes from "prop-types";
+const Box = ({ isFilled, isFood, isLastColumn, isLastRow }) => {
+  let boxColor = "white";
+  if (isFilled) {
+    boxColor = "red";
+  }
+  if (isFood) {
+    boxColor = "green";
+  }
   return (
     <div
       style={{
         height: `${BOX_SIZE}px`,
         width: `${BOX_SIZE}px`,
-        backgroundColor: isFilled ? "red" : "none",
+        backgroundColor: boxColor,
+        borderRight: isLastColumn ? "1px solid black" : "none",
+        borderBottom: isLastRow ? "1px solid black" : "none",
       }}
       className={css.box}
     ></div>
   );
+};
+
+Box.propTypes = {
+  isFilled: PropTypes.bool,
+  isFood: PropTypes.bool,
+  isLastColumn: PropTypes.bool,
+  isLastRow: PropTypes.bool,
 };
 
 export default Box;

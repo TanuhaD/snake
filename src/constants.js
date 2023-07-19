caf742dd9b6056aaba24876c914b599bbb0183cb
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-07-19 22:42:26 +0300

    constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,9 @@
 export const SNAKE_START_POSITION = [
   {
+    x: 0,
+    y: 2,
+  },
+  {
     x: 1,
     y: 2,
   },
@@ -13,11 +17,11 @@ export const SNAKE_START_POSITION = [
   },
 ];
 
-export const PLAYING_FIELD_WIDTH = 10;
+export const PLAYING_FIELD_WIDTH = 20;
 export const PLAYING_FIELD_HEIGHT = 15;
 export const BOX_SIZE = 20;
-export const DIRECTIONONS = {
-  left: 0,
+export const DIRECTIONS = {
+  left: 4,
   right: 2,
   up: 1,
   down: 3,

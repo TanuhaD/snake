074d376cc0bde82a6762825afe7357c81daef48b
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-07-21 19:46:27 +0300

    Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,18 +1,18 @@
 import css from "./Modal.module.css";
 import PropTypes from "prop-types";
-const Modal = ({ setRestart }) => {
+const Modal = ({ restartGame }) => {
   return (
     <div className={css.overlay}>
       <div className={css.modal}>
         <p>Game Over</p>
-        <button onClick={() => setRestart(true)}>Restart Game</button>
+        <button onClick={() => restartGame()}>Restart Game</button>
       </div>
     </div>
   );
 };
 
 Modal.propTypes = {
-  setRestart: PropTypes.func,
+  restartGame: PropTypes.func,
 };
 
 export default Modal;

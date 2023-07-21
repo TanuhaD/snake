63641744ab9dbfc34d69d46cdc9e86e2b0741792
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-07-21 19:45:54 +0300

    Game.jsx

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,119 +1,44 @@
 import { useEffect, useState } from "react";
 import PlayingField from "../PlayingField/PlayingField";
 import css from "./Game.module.css";
-import {
-  DIRECTIONS,
-  PLAYING_FIELD_HEIGHT,
-  PLAYING_FIELD_WIDTH,
-  SNAKE_START_POSITION,
-} from "../../constants";
+import { DIRECTIONS, SNAKE_START_POSITION } from "../../constants";
 import Modal from "../Modal/Modal";
+import {
+  getDirection,
+  getNewFoodCoords,
+  getScore,
+  hadleChangeDirectionSnake,
+  moveSnake,
+} from "../../utils/utils";
+import Controls from "../Controls/Controls";
 
-const randomNum = (min, max) => {
-  return Math.floor(Math.random() * (max + 1 - min) + min);
-};
-
-const isInSnake = (snake, coords) => {
-  return snake.some((section) => {
-    return section.x === coords.x && section.y === coords.y;
-  });
-};
-const getNewFoodCoords = (snake) => {
-  let newCoords;
-  do {
-    newCoords = {
-      x: randomNum(0, PLAYING_FIELD_WIDTH - 1),
-      y: randomNum(0, PLAYING_FIELD_HEIGHT - 1),
-    };
-  } while (isInSnake(snake, newCoords));
-  return newCoords;
-};
-const moveSnake = (
-  prevSnake,
-  foodCoords,
-  setFoodCoords,
-  direction,
-  setIsGameOver
-) => {
-  const newSnake = [...prevSnake];
-  const head = newSnake.at(-1);
-
-  switch (direction) {
-    case DIRECTIONS.left:
-      if (head.x - 1 < 0 || isInSnake(newSnake, { x: head.x - 1, y: head.y })) {
-        setIsGameOver(true);
-        return prevSnake;
-      }
-      newSnake.push({ ...head, x: head.x - 1 });
-      break;
-    case DIRECTIONS.right:
-      if (
-        head.x + 1 >= PLAYING_FIELD_WIDTH ||
-        isInSnake(newSnake, { x: head.x + 1, y: head.y })
-      ) {
-        setIsGameOver(true);
-        return prevSnake;
-      }
-      newSnake.push({ ...head, x: head.x + 1 });
-      break;
-    case DIRECTIONS.up:
-      if (head.y - 1 < 0 || isInSnake(newSnake, { y: head.y - 1, x: head.x })) {
-        setIsGameOver(true);
-        return prevSnake;
-      }
-      newSnake.push({ ...head, y: head.y - 1 });
-      break;
-    case DIRECTIONS.down:
-      if (
-        head.y + 1 >= PLAYING_FIELD_HEIGHT ||
-        isInSnake(newSnake, { y: head.y + 1, x: head.x })
-      ) {
-        setIsGameOver(true);
-        return prevSnake;
-      }
-      newSnake.push({ ...head, y: head.y + 1 });
-      break;
-  }
-  const newHead = newSnake.at(-1);
-  if (newHead.x === foodCoords.x && newHead.y === foodCoords.y) {
-    const newFoodCoords = getNewFoodCoords(newSnake);
-    setFoodCoords(newFoodCoords);
-  } else {
-    newSnake.shift();
-  }
-  return newSnake;
-};
 const Game = () => {
   const [snake, setSnake] = useState(SNAKE_START_POSITION);
   const [direction, setDirection] = useState(DIRECTIONS.right);
   const [isGameOver, setIsGameOver] = useState(false);
-  const [restart, setRestart] = useState(false);
-  const [maxScore, setMaxScore] = useState(() => {
-    return +localStorage.getItem("maxScore");
-  });
+  const [isPaused, setIsPaused] = useState(false);
+  const [maxScore, setMaxScore] = useState(
+    () => +localStorage.getItem("maxScore")
+  );
   const [foodCoords, setFoodCoords] = useState(() =>
     getNewFoodCoords(SNAKE_START_POSITION)
   );
-  const [changeDirectionByKeyboard, setChangeDirectionByKeyboard] =
-    useState(null);
+
+  const restartGame = () => {
+    setSnake(SNAKE_START_POSITION);
+    setDirection(DIRECTIONS.right);
+    setFoodCoords(getNewFoodCoords(SNAKE_START_POSITION));
+    setIsGameOver(false);
+  };
 
   useEffect(() => {
-    if (restart) {
-      setSnake(SNAKE_START_POSITION);
-      setDirection(DIRECTIONS.right);
-      setFoodCoords(getNewFoodCoords(SNAKE_START_POSITION));
-      setIsGameOver(false);
-      setRestart(false);
-    }
-  }, [restart]);
-  useEffect(() => {
     if (isGameOver) {
-      if (maxScore === 0 || snake.length * 10 > maxScore) {
-        localStorage.setItem("maxScore", snake.length * 10);
-        setMaxScore(snake.length * 10);
+      if (maxScore === 0 || getScore(snake.length) > maxScore) {
+        localStorage.setItem("maxScore", getScore(snake.length));
+        setMaxScore(getScore(snake.length));
       }
     }
-    if (!isGameOver) {
+    if (!isGameOver && !isPaused) {
       setSnake((prevSnake) => {
         return moveSnake(
           prevSnake,
@@ -125,7 +50,7 @@ const Game = () => {
       });
     }
     let intervalId;
-    if (!isGameOver) {
+    if (!isGameOver && !isPaused) {
       intervalId = setInterval(() => {
         setSnake((prevSnake) => {
           return moveSnake(
@@ -141,40 +66,18 @@ const Game = () => {
     return () => {
       clearInterval(intervalId);
     };
-  }, [direction, isGameOver]);
-
-  const hadleChangeDirectionSnake = (newDirection) => {
-    if (
-      (newDirection % 2 === 0 && direction % 2 === 0) ||
-      (newDirection % 2 !== 0 && direction % 2 !== 0)
-    ) {
-      return;
-    }
-    setDirection(newDirection);
-  };
-
-  useEffect(() => {
-    if (changeDirectionByKeyboard) {
-      hadleChangeDirectionSnake(changeDirectionByKeyboard);
-      setChangeDirectionByKeyboard(null);
-    }
-  }, [changeDirectionByKeyboard]);
+  }, [direction, isGameOver, isPaused]);
 
   const handleKeyPress = (e) => {
     console.log(e.code);
-    switch (e.code) {
-      case "ArrowLeft":
-        setChangeDirectionByKeyboard(DIRECTIONS.left);
-        break;
-      case "ArrowRight":
-        setChangeDirectionByKeyboard(DIRECTIONS.right);
-        break;
-      case "ArrowUp":
-        setChangeDirectionByKeyboard(DIRECTIONS.up);
-        break;
-      case "ArrowDown":
-        setChangeDirectionByKeyboard(DIRECTIONS.down);
-        break;
+    if (e.code === "Escape") {
+      setIsPaused((prevState) => {
+        return !prevState;
+      });
+    }
+    const newDirection = getDirection(e.code);
+    if (newDirection) {
+      hadleChangeDirectionSnake({ newDirection, setDirection });
     }
   };
 
@@ -185,9 +88,9 @@ const Game = () => {
 
   return (
     <div className={css.container}>
-      {isGameOver && <Modal setRestart={setRestart} />}
+      {isGameOver && <Modal restartGame={restartGame} />}
       <div className={css.box}>
-        <div className={css.count}>{snake.length * 10}</div>
+        <div className={css.count}>{getScore(snake.length)}</div>
         <div>{maxScore}</div>
         <PlayingField
           snake={snake}
@@ -196,39 +99,7 @@ const Game = () => {
           foodCoords={foodCoords}
         />
       </div>
-      <div className={css.wrapperBtn}>
-        <button
-          className={css.controlBtn}
-          type="button"
-          onClick={() => hadleChangeDirectionSnake(DIRECTIONS.left)}
-        >
-          left
-        </button>
-        <div className={css.boxBtn}>
-          <button
-            className={css.controlBtn}
-            type="button"
-            onClick={() => hadleChangeDirectionSnake(DIRECTIONS.up)}
-          >
-            up
-          </button>
-          <button
-            className={css.controlBtn}
-            type="button"
-            onClick={() => hadleChangeDirectionSnake(DIRECTIONS.down)}
-          >
-            down
-          </button>
-        </div>
-
-        <button
-          className={css.controlBtn}
-          type="button"
-          onClick={() => hadleChangeDirectionSnake(DIRECTIONS.right)}
-        >
-          right
-        </button>
-      </div>
+      <Controls setDirection={setDirection} />
     </div>
   );
 };
